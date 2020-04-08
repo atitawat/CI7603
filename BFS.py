fcf48844241358a4c9120d4d47b26af3a232d782
atitawat <atitawat.p@ku.th>
2020-04-09 06:56:42 +0700

    update

--- BFS.py
+++ BFS.py
@@ -1,19 +1,32 @@
 #Initial
 
-#def bfs(node, graph,):
-
-#def dfs(node, graph);
-
-def readTestFile(fname):
+def read_test_file(fname) -> dict:
+    '''taken file then return dict {vertex: [connecting vertices]}'''
     lines = []
-    with open(fname, 'r') as f:
-        for line in f:
+    vertex = {}
+
+    with open(fname, 'r') as file:
+        for line in file:
             lines.append(line.rstrip())
+
+    vertex = lambda lines: {lines[0]: lines[1:-1]}
+
+    return vertex
+
+class Vertex:
+    
+   def __init__(self, name, neighbors, distance, color):
+      self.name = name
+      self.neighbors = [next for node in neighbors]
+
+
+
+
     
 
-def main():
+'''def main():
     dir = input('Enter your test file:')
-    readTestFile(dir)
+    read_test_file(dir)
 
 if __name__ == "__main__":
-    main()
+    main()'''
